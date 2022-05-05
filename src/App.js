3141a94125353c0c8278cd310ddcd98282f6448a
Dan <dannorthcott8@gmail.com>
2022-05-05 15:09:33 +0100

    core criteria

--- src/App.js
+++ src/App.js
@@ -1,60 +1,69 @@
-import "./App.css"
+import "./App.css";
+import { useState } from "react";
 
-function App() { 
+function App() {
+  let [count1, setCount1] = useState(0);
+  let [operator, setOperator] = useState("+");
+  let [count2, setCount2] = useState(0);
+  let [answer, setAnswer] = useState(0);
+
+  function calcAnswer() {
+    setAnswer(eval(count1 + operator + count2));
+  }
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{count1}</p>
+        <div className="numbers">
+          <button onClick={() => setCount1((count1 = 1))}>1</button>
+          <button onClick={() => setCount1((count1 = 2))}>2</button>
+          <button onClick={() => setCount1((count1 = 3))}>3</button>
+          <button onClick={() => setCount1((count1 = 4))}>4</button>
+          <button onClick={() => setCount1((count1 = 5))}>5</button>
+          <button onClick={() => setCount1((count1 = 6))}>6</button>
+          <button onClick={() => setCount1((count1 = 7))}>7</button>
+          <button onClick={() => setCount1((count1 = 8))}>8</button>
+          <button onClick={() => setCount1((count1 = 9))}>9</button>
+          <button onClick={() => setCount1((count1 = 0))}>0</button>
+          <button onClick={() => setCount1((count1 = 0))}>Clear</button>
         </div>
-        
-        <div className="panel">
-          <p>+</p>
-          <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{operator}</p>
+        <div className="numbers">
+          <button onClick={() => setOperator((operator = "+"))}>+</button>
+          <button onClick={() => setOperator((operator = "-"))}>-</button>
+          <button onClick={() => setOperator((operator = "*"))}>*</button>
+          <button onClick={() => setOperator((operator = "/"))}>÷</button>
         </div>
-        <div className="panel answer">
-          <p>0</p>
-          <div>
-            <button>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{count2}</p>
+        <div className="numbers">
+          <button onClick={() => setCount2((count2 = 1))}>1</button>
+          <button onClick={() => setCount2((count2 = 2))}>2</button>
+          <button onClick={() => setCount2((count2 = 3))}>3</button>
+          <button onClick={() => setCount2((count2 = 4))}>4</button>
+          <button onClick={() => setCount2((count2 = 5))}>5</button>
+          <button onClick={() => setCount2((count2 = 6))}>6</button>
+          <button onClick={() => setCount2((count2 = 7))}>7</button>
+          <button onClick={() => setCount2((count2 = 8))}>8</button>
+          <button onClick={() => setCount2((count2 = 9))}>9</button>
+          <button onClick={() => setCount2((count2 = 0))}>0</button>
+          <button onClick={() => setCount2((count2 = 0))}>Clear</button>
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{answer}</p>
+        <div>
+          <button onClick={calcAnswer}>=</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
